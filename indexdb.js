272cef2b2134484078028a5bc9048dc518574fd5
ismaelcallerojas <ismaelcallerojas@gmail.com>
2022-06-02 20:35:14 -0400

    Crud indexeddb

--- indexdb.js
+++ indexdb.js
@@ -21,6 +21,46 @@ dbConnection.onupgradeneeded = (e) => {
 dbConnection.onerror = (error) =>{
     console.log(error);
 }
+
+//Insertar
+const insertar=(informacion)=>{
+    //lectura de tablas
+    var transaccion=db.transaction("articulo","readwrite");
+    const objeto= transaccion.objectStore('articulo');
+    //insertar en el objeto
+    const cargaInfo=objeto.add(informacion);
+    console.log("cargar info",cargaInfo);
+}
+
+//Eliminar
+//Actualizar
+const actualizar=(informacion)=>{
+    //lectura de tablas
+    var transaccion=db.transaction("articulo","readwrite");
+    const objeto= transaccion.objectStore('articulo');
+    //
+    const cargaInfo=objeto.put(informacion);
+}
+
+//devolver
+const devolver = ()=>{
+    db = dbConnection.result;
+    //lectura de tablas
+    var transaccion=db.transaction("articulo","readonly");
+    const objeto= transaccion.objectStore('articulo');
+    console.log(objeto)
+    //iterar los elementos de articulos
+    const cursor=objeto.openCursor();
+    cursor.onsuccess=(e)=>{
+        const c=e.target.result;
+        if (c) {
+            console.log(c.value);
+        } else {
+            console.log("No existe datos");
+        }
+    }
+}
+
 // https://developer.mozilla.org/es/docs/Web/API/IDBObjectStore/add
 // https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore/get
 // https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore/delete
